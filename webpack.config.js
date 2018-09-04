bb9481b0d5df9ad7740b2774285c657796aa7e89
Pietro Peluso <pietro.peluso@dazn.com>
2018-09-04 14:44:00 +0100

    added css loader and some more dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,23 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: '[name]___[local]___[hash:base64:5]',
+                            camelCase: true,
+                            url: true,
+                            import: true,
+                        },
+                    },
+                ],
             }
         ]
     },
